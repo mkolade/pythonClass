ea6f981a9a210212ba57abf27c39dccd5fd725c4
moshoodmohammed <mohammedamoshood@gmail.com>
2024-01-19 13:04:37 +0100

    #Built-in functions

--- 02-dataTypes/numeric.py
+++ 02-dataTypes/numeric.py
@@ -1,3 +1,4 @@
+import math
 #Numeric Data types
 
 #integers
@@ -9,4 +10,22 @@ print(isinstance(int2, int), "\n")# True
 #float
 myFloat = 3.72
 float2 = float(2.33)
-print(type(myFloat))# <class 'float'>
+print(type(myFloat) , "\n")# <class 'float'>
+
+#Complex numbers
+compNum = 3 + 7j
+print(type(compNum))# <class 'complex'>
+print(compNum.real)# 3.0
+print(compNum.imag)# 7.0
+print(type(compNum.real), "\n")# <class 'float'>
+
+#Built-in functions
+print(abs(myFloat))# 3.72
+print(abs(myFloat * -2))# 7.44
+print(round(myFloat))# 4
+print(round(myFloat, 1), "\n")# 3.7
+#with math module imported,
+print(round(math.pi,3))#3.142
+print(math.sqrt(64))# 8.0
+print(math.floor(myFloat))#3
+print(math.ceil(myFloat))#4
