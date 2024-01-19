810e0541fb86ab49e1ba24274010e3fb935b900a
moshoodmohammed <mohammedamoshood@gmail.com>
2024-01-19 12:05:44 +0100

    Creating Menu

--- 02-dataTypes/useStringMethd.py
+++ 02-dataTypes/useStringMethd.py
@@ -1,2 +1,7 @@
 #Creating Menu
 
+title = "Menu"
+print(title.center(20,"="))
+print("Coke".rjust(6,".") + "$1".ljust(4))
+print("Bread".rjust(6,".") + "$6".ljust(4))
+print("Beans".rjust(6,".") + "$5".ljust(4))
