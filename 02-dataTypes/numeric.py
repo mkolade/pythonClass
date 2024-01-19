c40447748260b67005cd0a805fe9c456190e1675
moshoodmohammed <mohammedamoshood@gmail.com>
2024-01-19 12:45:26 +0100

    integers and float

--- 02-dataTypes/numeric.py
+++ 02-dataTypes/numeric.py
@@ -1,3 +1,12 @@
 #Numeric Data types
 
 #integers
+myInt = 80
+int2 = int(22)
+print(type(myInt))# <class 'int'>
+print(isinstance(int2, int), "\n")# True
+
+#float
+myFloat = 3.72
+float2 = float(2.33)
+print(type(myFloat))# <class 'float'>
