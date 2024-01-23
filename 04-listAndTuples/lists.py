7523b9e7f92785c91defaa90a4058424f1af1ee1
moshoodmohammed <mohammedamoshood@gmail.com>
2024-01-23 23:35:15 +0100

    #LISTS

---
+++ 04-listAndTuples/lists.py
@@ -0,0 +1,16 @@
+#LISTS
+
+firstList = ["Mali",33,False]
+secondList = ["Mali","Teqiat","Dave"]
+emptyList = []
+
+print("Mali" in firstList)#True
+print(False in emptyList)#False
+
+print(firstList[2])#False
+print(firstList[-2])#33
+print(secondList[-2])#Teqiat
+
+
+print(secondList[0:2])#['Mali', 'Teqiat']
+
