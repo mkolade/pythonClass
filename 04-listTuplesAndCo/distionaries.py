197fc8d2f21e710d6b98ab64570e28a3f2f9831f
moshoodmohammed <mohammedamoshood@gmail.com>
2024-01-26 15:58:23 +0100

    #other methods

--- 04-listTuplesAndCo/distionaries.py
+++ 04-listTuplesAndCo/distionaries.py
@@ -6,5 +6,17 @@ firstDict = {
     "Class": "Baller"
 }
 secondDict = dict(Name="Akolade", Class = "Baller")
-print(firstDict)
-print(secondDict)
+# print(firstDict)#{'Name': 'Akolade', 'Class': 'Baller'}
+# print(secondDict)#{'Name': 'Akolade', 'Class': 'Baller'}
+
+#access dict items 
+print(firstDict["Class"])
+print(secondDict.get("Class"))
+
+#other methods
+#all keys
+print(firstDict.keys())#(['Name', 'Class'])
+#all values
+print(firstDict.values())#dict_values(['Akolade', 'Baller']) 
+#all items
+print(firstDict.items())#dict_items([('Name', 'Akolade'), ('Class', 'Baller')])
