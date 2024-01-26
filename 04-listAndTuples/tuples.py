4d53513ad8b97084b3c144c885b3b65d2629df4f
moshoodmohammed <mohammedamoshood@gmail.com>
2024-01-26 15:17:51 +0100

    #TUPLE - unchangeable form of lists
    #To create a tuple

---
+++ 04-listAndTuples/tuples.py
@@ -0,0 +1,10 @@
+#TUPLE - unchangeable form of lists
+
+#To create a tuple
+firstTuple = tuple(('Hello', 'Mother-Fucker'))
+secondTuple = tuple(('My', 'Name', 'Is'))
+print(secondTuple)#('My', 'Name', 'Is')
+#print(type(secondTuple))#<class 'tuple'>
+
+#To change the contents of a tuple, you gotta get creative
+tuListOne = list(firstTuple)
