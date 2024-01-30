664af1b771583f890f09fd55e7435cda1a3e7a3b
moshoodmohammed <mohammedamoshood@gmail.com>
2024-01-30 08:56:11 +0100

    created a new file to learn sets. #Sets - No duplicates allowed

---
+++ 04-listTuplesAndCo/sets.py
@@ -0,0 +1,6 @@
+#Sets - No duplicates allowed
+
+#Creating a set
+mySet1 = {1, 2, 3, 3, 4, 5, 5,7}
+myOtherSet = set((1, 2, 3, 3, 4, 5, 5,7))
+print(mySet1, type(myOtherSet))#{1, 2, 3, 4, 5, 7} <class 'set'>
