1a6102ee35a70a6e2089a3c6fc9fcda9f34b87a0
moshoodmohammed <mohammedamoshood@gmail.com>
2024-01-30 10:22:20 +0100

    updated rps game to include while loop

--- 03-userInputCtrl/RPSgame.py
+++ 03-userInputCtrl/RPSgame.py
@@ -12,28 +12,37 @@ print(RPS(2))
 # print(RPS.ROCK)
 # print(RPS['ROCK'].value)
 
-playerValue = input("Enter... \n1 for Rock,\n2 for Paper or \n3 for Scissors:\n\n")
-playerChoice = int(playerValue)
+playAgain = True
 
-if playerChoice < 1 or playerChoice > 3:
-    sys.exit("You must enter a value between 1 and 3")
+while playAgain: 
+    playerValue = input("\nEnter... \n1 for Rock,\n2 for Paper or \n3 for Scissors:\n\n")
+    playerChoice = int(playerValue)
 
-compValue = random.choice("123")
-compChoice = int(compValue)
+    if playerChoice < 1 or playerChoice > 3:
+        sys.exit("You must enter a value between 1 and 3")
 
-print("")
-print("You chose " + str(RPS(playerChoice)).replace("RPS.", '') + ".")
-print("Python chose " + str(RPS(compChoice)).replace("RPS.", '') + ".")
-print("")
+    compValue = random.choice("123")
+    compChoice = int(compValue)
 
+    print("\nYou chose " + str(RPS(playerChoice)).replace("RPS.", '') + ".")
+    print("Python chose " + str(RPS(compChoice)).replace("RPS.", '') + ".\n")
 
-if playerChoice == 1 and compChoice == 3:
-    print("🎉 You win!")
-elif playerChoice == 2 and compChoice == 1:
-    print("🎉 You win!")
-elif playerChoice == 3 and compChoice == 2:
-    print("🎉 You win!")
-elif playerChoice == compChoice:
-    print("It's a draw 😲. Try again!")
-else :
-    print("Python wins! 🐍")
+    if playerChoice == 1 and compChoice == 3:
+        print("🎉 You win!")
+    elif playerChoice == 2 and compChoice == 1:
+        print("🎉 You win!")
+    elif playerChoice == 3 and compChoice == 2:
+        print("🎉 You win!")
+    elif playerChoice == compChoice:
+        print("It's a draw 😲. Try again!")
+    else :
+        print("Python wins! 🐍 \n")
+
+    playAgain = input("Wanna keep playing?\n Enter Y for yes\n Q to quit\n\n")
+    if(playAgain == "y".lower()):
+        playAgain = True
+    else:
+        print("🎉🎉🙌🎉🎉")
+        print("Thanks for playing")
+        playAgain = False
+sys.exit("I hope we see you again")
