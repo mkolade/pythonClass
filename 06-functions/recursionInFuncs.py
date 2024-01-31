e21fe58425b5bd7a8adab505291fbe463e3da5c4
moshoodmohammed <mohammedamoshood@gmail.com>
2024-01-31 09:40:05 +0100

    To get 9 and 10, yea i know i bungled things up a bit

--- 06-functions/recursionInFuncs.py
+++ 06-functions/recursionInFuncs.py
@@ -1,11 +1,12 @@
 #defining a recursive function
 
 def firstRecursion(num):
-    if num >= 9:
-        return num
     myTotal = num + 1
-    print(myTotal)
-    firstRecursion(num + 1)
-    return 
+    if num >= 9:
+        return ("(" + str(num) + " " + str(myTotal) + (')'))
+    print("(" + str(num) + " " + str(myTotal) + (')'))#(0 1) (1 2) (2 3) (3 4) (4 5) (5 6) (6 7) (7 8) (8 9)
+    return firstRecursion(num + 1)
 
-firstRecursion(0)
+#To get 9 and 10, yea i know i bungled things up a bit
+get9and10 = firstRecursion(0)
+print(get9and10)
