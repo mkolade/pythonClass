277b0913f80ae87ca08085f9cb444733a7ae5ffc
moshoodmohammed <mohammedamoshood@gmail.com>
2024-02-01 20:24:54 +0100

    def anotherFunc():

--- 07-scopes/scopes.py
+++ 07-scopes/scopes.py
@@ -1,9 +1,13 @@
 name = "dave"
  
-def printStuff():
+def printStuff(name):
     color = "Red"
-    print(name)#dave
-    print(color)#color
+    print(name)#Akolade
+    print(color)#Red
 
 #print(color)#NameError: name 'color' is not defined
-printStuff()
+printStuff("Akolade")
+
+def anotherFunc():
+    printStuff("Atolagbe")# Atolagbe Red
+anotherFunc()
