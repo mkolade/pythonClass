6c923ce86e63c4a75242b8e88996ae105d4153c7
moshoodmohammed <mohammedamoshood@gmail.com>
2024-02-01 20:28:24 +0100

    #Nested function

--- 07-scopes/scopes.py
+++ 07-scopes/scopes.py
@@ -1,13 +1,21 @@
 name = "dave"
  
-def printStuff(name):
-    color = "Red"
-    print(name)#Akolade
-    print(color)#Red
+# def printStuff(name):
+#     color = "Red"
+#     print(name)#Akolade
+#     print(color)#Red
 
-#print(color)#NameError: name 'color' is not defined
-printStuff("Akolade")
+# #print(color)#NameError: name 'color' is not defined
+# printStuff("Akolade")
 
+# def anotherFunc():
+#     printStuff("Atolagbe")# Atolagbe Red
+# anotherFunc()
+
+#Nested function
 def anotherFunc():
+    color = "Red"
+    def printStuff(name):
+        print(name, color)
     printStuff("Atolagbe")# Atolagbe Red
 anotherFunc()
