91a79f58351986fff740d1dbd3965b88352c6fd3
moshoodmohammed <mohammedamoshood@gmail.com>
2024-02-10 13:06:52 +0100

    done with 5th edit on rps game

--- 03-userInputCtrl/RPSgame5th.py
+++ 03-userInputCtrl/RPSgame5th.py
@@ -2,56 +2,73 @@ import sys
 import random
 from enum import Enum
 
-gameCount = 0
-def play_rps():
-    class RPS(Enum):
-        ROCK = 1
-        PAPER = 2
-        SCISSORS = 3
+def rps():
+    gameCount = 0
+    playerWins = 0
+    pythonWins = 0
+    def play_rps():
+        nonlocal playerWins
+        nonlocal pythonWins
 
-    # print(RPS['ROCK'])
-    # print(RPS.ROCK)
-    # print(RPS['ROCK'].value)
-    playerValue = input("\nEnter... \n1 for Rock 🥌,\n2 for Paper📜 or \n3 for Scissors ✂ :\n\n")
-    if playerValue not in ["1", "2", "3"]:
-        print("You must enter a value between 1 and 3 ‼‼")
-        play_rps()
-    playerChoice = int(playerValue)
+        class RPS(Enum):
+            ROCK = 1
+            PAPER = 2
+            SCISSORS = 3
 
-    compValue = random.choice("123")
-    compChoice = int(compValue)
+        # print(RPS['ROCK'])
+        # print(RPS.ROCK)
+        # print(RPS['ROCK'].value)
+        playerValue = input("\nEnter... \n1 for Rock 🥌,\n2 for Paper📜 or \n3 for Scissors ✂ :\n\n")
+        if playerValue not in ["1", "2", "3"]:
+            print("You must enter a value between 1 and 3 ‼‼")
+            play_rps()
+        playerChoice = int(playerValue)
 
-    print("\nYou chose " + str(RPS(playerChoice)).replace("RPS.", '') + ".")
-    print("Python chose " + str(RPS(compChoice)).replace("RPS.", '') + ".\n")
+        compValue = random.choice("123")
+        compChoice = int(compValue)
 
-    def getWinner(player, computer):  
-        if player == 1 and computer == 3:
-            return "🎉 You win!"
-        elif player == 2 and computer == 1:
-            return "🎉 You win!"
-        elif player == 3 and computer == 2:
-            return "🎉 You win!"
-        elif player == computer:
-            return "It's a draw 😲. Try again!"
-        else :
-            return "Python wins! 🐍 \n"
+        print("\nYou chose " + str(RPS(playerChoice)).replace("RPS.", '') + ".")
+        print("Python chose " + str(RPS(compChoice)).replace("RPS.", '') + ".\n")
 
-    theWinner = getWinner(playerChoice,compChoice)
-    print(theWinner)
-    global gameCount
-    gameCount += 1
-    print("\n\nGames played: " + str(gameCount))
-    while True:
-        playAgain = input("Wanna keep playing?\n Enter Y for yes\n Q to quit\n\n")
-        if(playAgain.lower() not in ["y", "q"]):
+        def getWinner(player, computer): 
+            nonlocal pythonWins
+            nonlocal playerWins 
+            if player == 1 and computer == 3:
+                playerWins += 1
+                return "🎉 You win!"
+            elif player == 2 and computer == 1:
+                playerWins += 1
+                return "🎉 You win!"
+            elif player == 3 and computer == 2:
+                playerWins += 1
+                return "🎉 You win!"
+            elif player == computer:
+                return "It's a draw 😲. Try again!"
+            else :
+                pythonWins += 1
+                return "Python wins! 🐍 \n"
 
-            continue
+        theWinner = getWinner(playerChoice,compChoice)
+        print(theWinner)
+        nonlocal gameCount
+        gameCount += 1
+        print("\n\nGames played: " + str(gameCount))
+        print("\n\nPlayer wins: " + str(playerWins))
+        print("Python wins: " + str(pythonWins))
+        while True:
+            playAgain = input("Wanna keep playing?\n Enter Y for yes\n Q to quit\n\n")
+            if(playAgain.lower() not in ["y", "q"]):
+
+                continue
+            else:
+                break
+        if(playAgain.lower() == "y"):
+            play_rps()
         else:
-            break
-    if(playAgain.lower() == "y"):
-        play_rps()
-    else:
-        print("🎉🎉🙌🎉🎉")
-        print("Thanks for playing")
-        sys.exit("I hope we see you again")
-play_rps()
+            print("🎉🎉🙌🎉🎉")
+            print("Thanks for playing")
+            sys.exit("I hope we see you again")
+    return play_rps
+
+startGame = rps()
+startGame()
